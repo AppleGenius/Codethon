e06d0b5ea133941a20d9171086f0be144dd4a5c2
Jeff <AppleGenius_Liu@me.com>
2014-03-05 16:46:33 +0800

    code everyday

--- CPL/4.1t/4.1.c
+++ CPL/4.1t/4.1.c
@@ -49,3 +49,40 @@ int strindex(char s[], char t[])
 
 	return -1;
 }
+
+int strindexL(char s[], char t[])
+{
+	int i = 0, count = -1, k;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (int j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++);
+		if (k > 0 && t[k] == '\0')
+			return i;
+	}
+	return -1;
+}
+
+
+double atof(char s[])
+{
+	double val, power;
+	int i, sign;
+
+	for (i = 0; isspace(s[i]); i++);
+	sign = (s[i] == '-') ? -1 : 1;
+
+	if (s[i] == '+' || s[i] == '-')
+		i++;
+	for (val = 0.0; isdigit(s[i]); i++)
+		val = 10.0 * val + (s[i] - '0');
+	if (s[i] == '.')
+		i++;
+	for (power = 1.0; isdigit(s[i]); i++)
+	{
+		val = 10.0 * val + (s[i] - '0');
+		power *= 10;
+	}
+
+	return sign * val / power;
+}
