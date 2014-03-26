47a665d03d2cdb043912f5f36162cdaeb2a90c2b
Jeff <AppleGenius_Liu@me.com>
2014-03-26 16:36:31 +0800

    code everyday

--- CPL/CPL/test.c
+++ CPL/CPL/test.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
-struct key{
-	char * word;
-	int count;
-}keytab[] = {
-	"auto", 0,
-	"break", 0,
-	"case", 0,
-	"char", 0,
-	"const", 0,
-	"continue", 0,
-	"default", 0,
-};
+int main(int argc, const char *argv[])
+{
+	int c;
+
+	while ((c = getchar()) != EOF)
+	{
+		printf("%c\n", c);
+		switch (c)
+		{
+		case '0': case '1': case '2': case '3': case '4': case '5':case '6': case '7': case '8': case '9':
+			printf("Number!\n");
+			break;
+		case 'a': case 'b':
+			printf("Char!\n");
+			break;
+		default:
+			break;
+		}
+		printf("while\n");
+		//break;
+	}
+
+	printf("main\n");
+	return 0;
+}
